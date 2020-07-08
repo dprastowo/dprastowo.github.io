14e8195e9fd0a433c0588bfbae4b9cb4c8301b5e
Dwi Prastowo <dprastowo@gmail.com>
2020-07-09 04:59:37 +0700

    menyalin isi folder build

---
+++ precache-manifest.dee4d05772360265dd7616f16d4f660a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b8e7023f35b088cf78af",
+    "url": "/static/js/main.b8e7023f.chunk.js"
+  },
+  {
+    "revision": "d0910af8e207b07d302b",
+    "url": "/static/js/1.d0910af8.chunk.js"
+  },
+  {
+    "revision": "b8e7023f35b088cf78af",
+    "url": "/static/css/main.108d33f3.chunk.css"
+  },
+  {
+    "revision": "994e08cb6d0b51146b1a695f184c132c",
+    "url": "/index.html"
+  }
+];
